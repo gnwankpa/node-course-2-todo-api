e947b86df8d707f76336d7d2e272beff06c96903
gnwankpa <gnwankpa@gmail.com>
2018-01-28 00:54:39 -0500

    deleting data

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+// deleteMany
+// db.collection('Todos').deleteMany({text:'Eat lunch'}). then((result) => {
+//     console.log(result);
+// })
+// db.collection('Users').deleteMany({name: 'Andrew'}).then((result) => {
+//     console.log(result);
+// })
+
+//deleteOne
+// db.collection('Todos').deleteOne({text: 'Something to do'}).then((result) => {
+//     console.log(result);
+// })
+db.collection('Users').deleteOne({_id: new ObjectID('5a6cdff81731234d70ff3b94')}).then((result) => {
+    console.log(result);
+})
+
+//findOneAndDelete
+// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+//     console.log(result);
+// })
+
+//db.close();
+//   db.close();
+
+
+});
