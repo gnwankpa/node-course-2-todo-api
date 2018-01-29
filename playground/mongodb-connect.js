e33115b898acf6dc65a589407bae1c1a58d82223
gnwankpa <gnwankpa@gmail.com>
2018-01-28 23:49:41 -0500

    updated gitignore

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,12 +1,7 @@
 // const MongoClient = require('mongodb').MongoClient
 const {MongoClient, ObjectID} = require('mongodb');
 
-var obj = new ObjectID();
-console.log(obj)
 
-var user = {name: 'andrew', age: 25};
-var {name} = user;
-console.log(name)
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
