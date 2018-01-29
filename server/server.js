2ba1ed8fe3717594ad04b6e685571b75b0d01103
gnwankpa <gnwankpa@gmail.com>
2018-01-29 11:53:56 -0500

    findBy ID route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 
 var app = express();
@@ -33,6 +35,45 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 })
+
+app.get('/todos/:id' , (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        console.log('ID not valid');
+        return res
+        .status(404)
+        .send();
+
+    } Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res
+            .status(404)
+            .send();
+            
+
+        } console.log('Todo by ID:', todo);
+        res
+        .status(200)
+        .send(todo);
+        
+        
+
+    }).catch((e) => {
+        console.log(e);
+        res.status(400).send();
+    });
+
+    //valid id using isValid
+        //404 send back empty send
+
+    // findById
+        // success
+            //if todo - send it back
+            // if no todo - send back 404 with  empty body
+    // error
+        // 400 - and send empty body back 
+
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
